212dcdf503a20756dc6006eb3e83de28759859a4
@gncrysr <gncrysr@gmail.com>
2022-11-09 19:26:18 +0300

    Update

--- ProjectHook/Assets/Scripts/SpawnerMechanics/PlatformManager.cs
+++ ProjectHook/Assets/Scripts/SpawnerMechanics/PlatformManager.cs
@@ -7,13 +7,14 @@ public class PlatformManager : ScriptableObject
 {
     [SerializeField] List<GameObject> basicPlatformList, advancedPlatformList;
     [SerializeField] List<GameObject> detailsList;
-    List<GameObject> allPlatformList;
+    List<GameObject> allPlatformList = new List<GameObject>();
 
     private void SetAllPlatforms() {
         foreach (GameObject item in basicPlatformList)
             allPlatformList.Add(item);
         foreach (GameObject item in advancedPlatformList)
             allPlatformList.Add(item);
+        Debug.Log(allPlatformList.Count);
     }
     public GameObject GetRandom(List<GameObject> list){ return list[Random.Range(0, list.Count)]; }
     public List<GameObject> GetPlatformList() { if(allPlatformList.Count <= 0) SetAllPlatforms(); return allPlatformList; }
